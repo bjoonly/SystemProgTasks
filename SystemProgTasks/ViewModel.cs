b7dc47d6bc29908de126619e2c27471bc5507891
bjoonly <elumnaya@gmail.com>
2021-03-02 00:20:25 +0200

    Init

---
+++ SystemProgTasks/ViewModel.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SystemProgTasks
+{
+    class TextInfo : INotifyPropertyChanged
+    {
+        private int numberSentances;
+        private int numberInterrogativeSentances;
+        private int numberExclamationSentances;
+        private int numberCharacters;
+        private int numberWords;
+
+        public int NumberSentances { get { return numberSentances; } set { if (numberSentances != value) { numberSentances = value; OnPropertyChanged(); } } }
+        public int NumberInterrogativeSentances { get { return numberInterrogativeSentances; } set { if (numberInterrogativeSentances != value) { numberInterrogativeSentances = value; OnPropertyChanged(); } } }
+        public int NumberExclamationSentances { get { return numberExclamationSentances; } set { if (numberExclamationSentances != value) { numberExclamationSentances = value; OnPropertyChanged(); } } }
+        public int NumberCharacters { get { return numberCharacters; } set { if (numberCharacters != value) { numberCharacters = value; OnPropertyChanged(); } } }
+        public int NumberWords { get { return numberWords; } set { if (numberWords != value) { numberWords = value; OnPropertyChanged(); } } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    class ViewModel : INotifyPropertyChanged
+    {
+        private string text;
+        public string Text { get { return text; } set { if (text != value) { text = value; OnPropertyChanged(); } } }
+
+        private bool isSaving;
+        public bool IsSaving { get { return isSaving; } set { if (value != isSaving) { isSaving = value; OnPropertyChanged(); } } }
+
+
+        private bool isAddingNumberSentances;
+        private bool isAddingNumberInterrogativeSentances;
+        private bool isAddingNumberExclamationSentances;
+        private bool isAddingNumberCharacters;
+        private bool isAddingNumberWords;
+        public bool IsAddingNumberSentances { get { return isAddingNumberSentances; } set { if (isAddingNumberSentances != value) { isAddingNumberSentances = value; OnPropertyChanged(); } } }
+        public bool IsAddingNumberInterrogativeSentances { get { return isAddingNumberInterrogativeSentances; } set { if (isAddingNumberInterrogativeSentances != value) { isAddingNumberInterrogativeSentances = value; OnPropertyChanged(); } } }
+        public bool IsAddingNumberExclamationSentances { get { return isAddingNumberExclamationSentances; } set { if (isAddingNumberExclamationSentances != value) { isAddingNumberExclamationSentances = value; OnPropertyChanged(); } } }
+        public bool IsAddingNumberCharacters { get { return isAddingNumberCharacters; } set { if (isAddingNumberCharacters != value) { isAddingNumberCharacters = value; OnPropertyChanged(); } } }
+        public bool IsAddingNumberWords { get { return isAddingNumberWords; } set { if (isAddingNumberWords != value) { isAddingNumberWords = value; OnPropertyChanged(); } } }
+
+        private Command startCommand;
+        public ICommand StartCommand => startCommand;
+
+
+        private Command stopCommand;
+        public ICommand StopCommand => stopCommand;
+
+        CancellationTokenSource cancelTokenSource;
+        CancellationToken token;
+        private TextInfo info;
+        public TextInfo Info { get { return info; } set { if (value != info) { info = value; OnPropertyChanged(); } } }
+
+        public void Start()
+        {
+            try
+            {
+
+                if (token.IsCancellationRequested)
+                    return;
+                Task.Run(Calculate,token).ContinueWith(Save,token);
+                   
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void Stop()
+        {
+            cancelTokenSource.Cancel();
+
+        }
+
+        public void Save(Task t)
+        {
+            if (isSaving) { 
+            string path = "Report.txt";
+                using (StreamWriter sw = new StreamWriter(File.Create(path)))
+                {
+                    sw.WriteLine("Report");
+                    if (isAddingNumberSentances)
+                        sw.WriteLine("Number of sentances: " + Info.NumberSentances);
+                    if (isAddingNumberInterrogativeSentances)
+                        sw.WriteLine("Number of interrogative sentances: " + Info.NumberInterrogativeSentances);
+                    if (isAddingNumberExclamationSentances)
+                        sw.WriteLine("Number of exclamation sentances: " + Info.NumberExclamationSentances);
+                    if (isAddingNumberCharacters)
+                        sw.WriteLine("Number of characters: " + Info.NumberCharacters);
+                    if (isAddingNumberWords)
+                        sw.WriteLine("Number of words: " + Info.NumberWords);
+
+                }
+
+
+            }
+        }
+
+        public void Calculate()
+        {
+            try
+           {
+                Info.NumberSentances = 0;
+                Info.NumberExclamationSentances = 0;
+                Info.NumberInterrogativeSentances = 0;
+                Info.NumberCharacters = 0;
+                Info.NumberWords = 0;
+                string[] sentances = text.Split(new string[] { "...", ".", "?", "!", }, StringSplitOptions.RemoveEmptyEntries).AsParallel().WithCancellation(token).ToArray();
+                string[] words = text.Split(new char[] { ' ', '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+                Info.NumberSentances = sentances.Length;
+                Thread.Sleep(500);
+
+                Info.NumberWords = words.Length;
+                Thread.Sleep(500);
+
+                Info.NumberCharacters = text.Where(t => t != ' ').AsParallel().WithCancellation(token).Count();
+                Thread.Sleep(500);
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (token.IsCancellationRequested)
+                        return;
+
+                    if (text[i] == '!' && i > 0 && text[i - 1] != ' ')
+                        Info.NumberExclamationSentances++;
+                    else if (text[i] == '?' && i > 0 && text[i - 1] != ' ')
+                        Info.NumberInterrogativeSentances++;
+
+                }
+
+
+
+            }
+            catch ( OperationCanceledException ex){
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public ViewModel()
+        {
+            cancelTokenSource = new CancellationTokenSource();
+            token = cancelTokenSource.Token;
+            startCommand = new DelegateCommand(Start, () => !String.IsNullOrEmpty(Text));
+            stopCommand = new DelegateCommand(Stop);
+            PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == nameof(Text))
+                {
+                    startCommand.RaiseCanExecuteChanged();
+
+
+                }
+            };
+            Info = new TextInfo();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Task45/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using MessageBox = System.Windows.Forms.MessageBox;
+
+namespace Task45
+{
+
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        private int countFiles;
+        private int countDublicates;
+        private int countMoving;
+
+        public int CountFiles { get { return countFiles; } set { if (value != countFiles) { countFiles = value; OnPropertyChanged(); } } }
+        public int CountDublicates { get { return countDublicates; } set { if (value != countDublicates) { countDublicates = value; OnPropertyChanged(); } } }
+        public int CountMoving { get { return countMoving; } set { if (value != countMoving) { countMoving = value; OnPropertyChanged(); } } }
+        private string fromPath;
+       private string toPath;
+       public string FromPath { get { return fromPath; }set{ if (value != fromPath) { fromPath = value; OnPropertyChanged(); } } }
+       public string ToPath { get { return toPath; } set { if (value != toPath) { toPath = value; OnPropertyChanged(); } } }
+        List<FileInfo> originals = new List<FileInfo>();
+        private void fromButton_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                FromPath = fbd.SelectedPath;
+      
+        }
+
+        private void toButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                ToPath = fbd.SelectedPath;
+            
+
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Task.Run(Search).ContinueWith(FileTransfer);
+
+            }catch(Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+        }
+        public void FileTransfer(Task t)
+        {
+            foreach (var item in originals)
+            {                
+                File.Move(item.FullName, Path.Combine(ToPath, item.Name));
+                CountMoving++;
+            }
+            
+            CountDublicates = CountFiles - CountMoving;
+        }
+        public void Search()
+        {
+        
+            DirectoryInfo di = new DirectoryInfo(FromPath);
+            List<FileInfo> files = new List<FileInfo>(di.GetFiles());
+            int count;
+
+            CountFiles = files.Count;
+           
+            var dublicates = files.GroupBy(f => f.Length).Where(f =>f.Count() > 1).ToList();
+            foreach (var group in files.GroupBy(f => f.Length).Where(f => f.Count()== 1))
+            {
+                foreach (var item in group)
+                {
+
+                originals.Add(item);
+                
+                }
+
+            }
+            foreach (var g in dublicates)
+            {
+                
+                foreach (var file in g)
+                {
+                    count = 0;
+
+                    foreach (var item in g)
+                    {
+                        if (file.FullName == item.FullName)
+                            continue;
+                        using (StreamReader sr1 = new StreamReader(File.OpenRead(file.FullName)))
+                        {
+                            using (StreamReader sr2 = new StreamReader(File.OpenRead(item.FullName)))
+                            {
+                                string line1 = sr1.ReadToEnd();
+                                string line2 = sr2.ReadToEnd();
+                                if (line1 == line2)
+                                    count++;
+                                sr2.Close();
+                            }
+                            sr1.Close();
+
+                        }
+
+                    }
+                        if (count == 0)
+                        {
+                        originals.Add(file);
+     
+
+                    }
+                }
+
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
